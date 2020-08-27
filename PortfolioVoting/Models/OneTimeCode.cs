197f5574c4491c96aafb4ca4ddadd5dd0e6c7bc4
ofHillsAndLakes <conorohaleck@gmail.com>
2020-08-27 14:21:55 -0700

    Added Database code and methods

---
+++ PortfolioVoting/Models/OneTimeCode.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PortfolioVoting.Models
+{
+    public class OneTimeCode
+    {
+        public int OTCID { get; set; }
+
+        public string Code { get; set; }
+
+        public static OneTimeCode generateCode(AnonPoll poll)
+        {
+
+        }
+
+        public static OneTimeCode[] generateCode(int codeCount, AnonPoll poll)
+        {
+            OneTimeCode[] otc = new OneTimeCode[codeCount];
+
+            for(int i = 0; i < codeCount; i++)
+            {
+                otc[i] = generateCode(poll);
+            }
+
+            return otc;
+
+        }
+
+        public override string ToString()
+        {
+            return Code;
+        }
+    }
+}
